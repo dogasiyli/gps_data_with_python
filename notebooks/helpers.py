7ea9f1431551dec272bc03791fc7055c92bd5c46
dogasiyli-work-comp <dogasiyli@gmail.com>
2023-09-08 15:20:40 +0300

    keep the hard work on analyze parkur

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -1,6 +1,7 @@
 import os
 import gpxpy
 import json
+import numpy as np
 
 IDENTIFIERS_FILE = "identifiers.json"
 
@@ -37,3 +38,54 @@ def get_indices(center_idx, threshold, divisible_by):
     upper_limit = ((center_idx + threshold) // divisible_by + 1) * divisible_by
     idx_arr = range(lower_limit, upper_limit)
     return idx_arr
+
+def smooth_vector(input_vector, threshold):
+    input_vector = np.array(input_vector)
+    sorted_vector = np.sort(input_vector.flatten())
+    smoothed_vector = []
+    current_group = [sorted_vector[0]]
+
+    for i in range(1, len(sorted_vector)):
+        if sorted_vector[i] - sorted_vector[i - 1] <= threshold:
+            current_group.append(sorted_vector[i])
+        else:
+            if len(current_group) == 1:
+                smoothed_vector.append(current_group[0])
+            else:
+                median_value = np.median(current_group)
+                smoothed_vector.append(int(median_value))
+            current_group = [sorted_vector[i]]
+
+    if len(current_group) == 1:
+        smoothed_vector.append(current_group[0])
+    else:
+        median_value = np.median(current_group)
+        smoothed_vector.append(int(median_value))
+
+    return smoothed_vector
+
+def find_direction_change_indices(x, tresh=3):
+    vals_mul = x[0:-1]*x[1:]
+    idx = np.argwhere(vals_mul<0)
+    print("idx:", end=":(")
+    for i in idx:
+        mode = '?' 
+        if (x[i]<0 and x[i+1]>0):
+            mode = 'INC' 
+        if (x[i]>0 and x[i+1]<0):
+            mode = 'dec' 
+        print(f"{i}:{mode}",end=");(")
+    print(")")
+
+    smoothedidx = smooth_vector(np.squeeze(idx), tresh)
+    print(f"smoothed:tresh({tresh}):", end=":(")
+    for i in smoothedidx:
+        mode = '?' 
+        if (x[i]<0 and x[i+1]>0):
+            mode = 'INC' 
+        if (x[i]>0 and x[i+1]<0):
+            mode = 'dec' 
+        print(f"{i}:{mode}",end=");(")
+    print(")")
+
+    return smoothedidx
